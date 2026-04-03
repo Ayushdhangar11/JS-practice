32279cb403616fe781cc6ac3ad48964340841251
Ayushdhangar11 <ayushdhangar1114@gmail.com>
2026-04-03 18:55:28 +0530

    r

--- dbRELATIONSHIP/Models/user.js
+++ dbRELATIONSHIP/Models/user.js
@@ -5,7 +5,7 @@ main()
   .catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb://localhost:27017/relation");
+  
 }
 
 const userSchema = new mongoose.Schema({
